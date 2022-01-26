2c8cad46436ac77bf9b5301fb855f0e975520326
Carlos Hurtado <churtado@edrans.com>
2022-01-26 13:25:57 +0100

    Set ID for pages

--- statuspage/data_source_pages.go
+++ statuspage/data_source_pages.go
@@ -1,6 +1,7 @@
 package statuspage
 
 import (
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
@@ -13,35 +14,35 @@ func dataSourcePages() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"filter": dataSourceFiltersSchema(),
-			"name": {
+			"page_name": {
 				Description:  "the name of the page to",
 				Type:         schema.TypeString,
 				Required:     true,
 				ValidateFunc: validation.StringIsNotEmpty,
 			},
 			// Computed values
-			"pages": {
-				Type:     schema.TypeList,
-				Computed: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						// Required
+			// "pages": {
+			// 	Type:     schema.TypeList,
+			// 	Computed: true,
+			// 	Elem: &schema.Resource{
+			// 		Schema: map[string]*schema.Schema{
+			// 			// Required
 
-						// Optional
+			// 			// Optional
 
-						// Computed
-						"id": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"name": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
+			// 			// Computed
+			// 			"id": {
+			// 				Type:     schema.TypeString,
+			// 				Computed: true,
+			// 			},
+			// 			"name": {
+			// 				Type:     schema.TypeString,
+			// 				Computed: true,
+			// 			},
 						
-					},
-				},
-			},
+			// 		},
+			// 	},
+			// },
 		},
 	}
 }
@@ -51,7 +52,9 @@ func dataSourcePagesRead(d *schema.ResourceData, m interface{}) error {
 	providerConf := m.(*ProviderConfiguration)
 	statuspageClientV1 := providerConf.StatuspageClientV1
 	authV1 := providerConf.AuthV1
+	page_name := d.Get("page_name").(string)
 
+	fmt.Printf(page_name)
 	// res, _, err := statuspageClientV1.ComponentsApi.GetPagesPageIdComponents(authV1, d.Get("page_id").(string)).Execute()
 	req := statuspageClientV1.PagesApi.GetPages(authV1)
 
@@ -63,19 +66,28 @@ func dataSourcePagesRead(d *schema.ResourceData, m interface{}) error {
 	// 	return TranslateClientErrorDiag(err, "error querying component list")
 	// }
 
-	d.SetId(GenerateDataSourceHashID("DataSourcePages-", dataSourcePages(), d))
+	// d.SetId(GenerateDataSourceHashID("DataSourcePages-", dataSourcePages(), d))
 	resources := []map[string]interface{}{}
 
 	for _, r := range res  {
-		pages := map[string]interface{}{}
+		
 
+	
 		if _, ok := r.GetNameOk(); ok  {
-			pages["id"] = r.GetId()
-			pages["Name"] = r.GetName()
+			if r.GetName() == page_name{
+				d.SetId(r.GetId())
+				// pages["id"] = r.GetId()
+				// pages["name"] = r.GetName()
+				// resources = append(resources, pages)
+				break
+			} 
+			
+			
 
 
-			resources = append(resources, pages)
+			
 		}
+		
 
 	}
 
@@ -83,9 +95,9 @@ func dataSourcePagesRead(d *schema.ResourceData, m interface{}) error {
 		resources = ApplyFilters(f.(*schema.Set), resources, dataSourcePages().Schema["pages"].Elem.(*schema.Resource).Schema)
 	}
 
-	if err := d.Set("pages", resources); err != nil {
-		return err
-	}
+	// if err := d.Set("pages", resources); err != nil {
+	// 	return err
+	// }
 
 	return nil
 }
