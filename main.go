8b7a11395c623f9855a27910c1dd0947232578c7
Carlos Hurtado <churtado@edrans.com>
2022-03-02 11:32:53 +0100

    Remove debug config

--- main.go
+++ main.go
@@ -1,32 +1,15 @@
 package main
 
 import (
-	"context"
-	"flag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/sbecker59/terraform-provider-statuspage/statuspage"
-	"log"
 )
 
 func main() {
-	var debugMode bool
-	flag.BoolVar(&debugMode, "debug", true, "set to true to run the provider with support for debuggers like delve")
-	flag.Parse()
-
-	opts := &plugin.ServeOpts{
+	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
 			return statuspage.Provider()
 		},
-	}
-
-	if debugMode {
-		err := plugin.Debug(context.Background(), "sbecker59/statuspage", opts)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		return
-	}
-
-	plugin.Serve(opts)
+	})
 }
